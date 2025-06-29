fa3eeea359f68144d189e8cbceb396c49e92588c
Mohammedyassin22 <moyassin2004522@gmail.com>
2025-06-29 21:23:44 +0300

    I did BLL for the project.

---
+++ Infrastructure/Presistence/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+using Presistence.Repository;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presistence
+{
+    public class UnitOfWork(CPMSDbContext dbContext,ConcurrentDictionary<string,object> valuePairs) : IUnitOfWork
+    {
+        public  IGenericRebository<TEntity, TKey> GetRebository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        => (IGenericRebository<TEntity, TKey>)valuePairs.GetOrAdd(typeof(TEntity).Name, new GenericRepository<TEntity, TKey>(dbContext));
+
+        public async Task<int> SaveChangeAsync()
+        {
+            return await dbContext.SaveChangesAsync();
+        }
+    }
+}
