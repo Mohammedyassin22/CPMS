fdb6a46f9ba4268d30b9e44f72425034c6cd39d0
Mohammedyassin22 <moyassin2004522@gmail.com>
2025-07-01 18:21:49 +0300

    I worked in the shared class DTO

---
+++ Shared/InvoiceDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class InvoiceDto
+    {
+        public int Id { get; set; }
+        public DateTime Duration { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime ParckingRecord { get; set; }
+        public decimal payment { get; set; }
+    }
+}
---
+++ Shared/VehicleOwnerDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class VehicleOwnerDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumuber { get; set; }
+        public int PlateNumber { get; set; }
+    }
+}
