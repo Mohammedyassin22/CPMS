1747db3d480a12c22227d349dd1865ebdd96e92b
Mohammedyassin22 <moyassin2004522@gmail.com>
2025-07-06 02:55:16 +0300

    I added the car control unit.

---
+++ Infrastructure/Presentation/VehicleController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VehicleController(IServiceManager serviceManager):ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllVehcile()
+        {
+            var result =await serviceManager.VehicleService.GetAllVehiclesAsync();
+            if (result is null)
+                return BadRequest();
+            return Ok(result);  
+        }
+
+        [HttpGet("Type")]
+        public async Task <ActionResult<VehicleTypeDto>> GetAllVehicleBType(string VehicleType)
+        {
+           var result = await serviceManager.VehicleService.GetAllVehicleBTypeAsync(VehicleType);
+            if (result == null || !result.Any())
+                return NotFound();
+           return Ok(result);
+        }
+        [HttpGet("PlatNumber")]
+        public async Task<ActionResult<VehicleDto>> GetAllByNumber(string PlatNumber)
+        {
+            var result = await serviceManager.VehicleService.GetVehicleByNumberAsync(PlatNumber);
+            if (result == null)
+                return NotFound();
+            return Ok(result);
+        }
+        [HttpGet("Owner")]
+        public async Task<ActionResult<VehicleTypeDto>> GetAllVehicleBOwner(string Owner)
+        {
+            var result = await serviceManager.VehicleService.GetVehicleOwnersAsync(Owner);
+            if (result == null || !result.Any())
+                return NotFound();
+            return Ok(result);
+        }
+    }
+}
