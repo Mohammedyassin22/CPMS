1f0f79b9fcad048e70bf88a569539d941b6976b8
Mohammedyassin22 <moyassin2004522@gmail.com>
2025-07-29 17:03:39 +0300

    Refactoring main function and middleware

---
+++ CPMS/Extantion/Extantion.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service;
+using ServiceAbstraction;
+using Microsoft.OpenApi.Services;
+using Shared.ErrorModels;
+using Presistence;
+using CPMS.MiddleWare;
+using Domain;
+
+namespace CPMS.Extantion
+{
+    public static class Extantion
+    { 
+        public static IServiceCollection Register (this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+           services.AddEndpointsApiExplorer();
+           services.AddSwaggerGen();
+          
+           services.AddInfrastructure(configuration);
+         
+           services.AddScoped<IServiceManager, ServiceManager>();
+           services.AddAutoMapper(typeof(MappingAssemblyReference).Assembly);
+         
+           services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var errors = context.ModelState
+                        .Where(x => x.Value.Errors.Any())
+                        .Select(x => new ValidationError
+                        {
+                            field = x.Key,
+                            errors = x.Value.Errors.Select(e => e.ErrorMessage)
+                        }).ToList();
+
+                    var response = new ValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(response);
+                };
+            });
+            return services;
+        }
+
+        public static async Task<WebApplication> configurationmiddleware(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            using var scope = app.Services.CreateScope();
+            var dbintilaizer = scope.ServiceProvider.GetService<IDbIntilaizer>();
+            await dbintilaizer.IntilaizerAsync();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseMiddleware<GlobalErrorMiddleWare>();
+
+            app.MapControllers();
+            return app;
+        }
+    }
+}
---
+++ Infrastructure/Presistence/InfrastructureServices.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presistence
+{
+    public static class InfrastructureServices
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CPMSDbContext>(option =>
+            {
+                option.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IDbIntilaizer, DbIntilaizer>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
